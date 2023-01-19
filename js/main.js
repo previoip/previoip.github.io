64e250abd3ef5229093fe418625ebe7711b9b48b
Previo Prakasa <previo.prakasa@gmail.com>
2023-01-19 21:32:26 +0700

    build Thu, Jan 19, 2023  9:32:26 PM

--- js/main.js
+++ js/main.js
@@ -1,28 +1,52 @@
 
-function rmElementByCLassName(parent, elClassName) {
+function rmChildElementByCLassName(parent, elClassName) {
   const els = parent.getElementsByClassName(elClassName)
   while (els.length > 0) {
     els[0].parentNode.removeChild(els[0])
   }
 }
 
+
+function createInput(inputType, value) {
+  const el = document.createElement('input')
+  el.type = inputType
+  el.value = value
+  el.className = 'input-' + inputType
+  return el
+}
+
 function copyCodeblockButton(codeBlockElement) {
-  // Constructor functions, returns code block element
-  el = document.createElement('input')
-  el.type = 'button'
-  el.value = 'Copy'
-  el.classList = 'copy-button'
+  // Constructor functions, returns codeblock input -> button element
+  const el = createInput('button', 'Copy')
   el.onclick = () => {
-
+    // https://developer.mozilla.org/en-US/docs/Web/Security/User_activation
     let elCopy = codeBlockElement.cloneNode(true) // deep copy of codeblock element nodes
+    let tempValue = el.value
 
-    rmElementByCLassName(elCopy, 'ln'); // Removes line number
+    rmChildElementByCLassName(elCopy, 'ln'); // Removes line number
 
     navigator.clipboard.writeText(elCopy.innerText).then( 
       ()=>{
+        el.value = 'Copied!'
+        el.disabled = true
+        el.classList.add('disabled')
+        setTimeout(()=>{
+          el.value = tempValue
+          el.disabled = false
+          el.classList.remove('disabled')
+        }, 2000)
         console.log("Copied to clipboard successfully!");
+        
       },
       ()=>{
+        el.value = 'Failed!'
+        el.classList.add('failed')
+        el.disabled = true
+        setTimeout(()=>{
+          el.value = tempValue
+          el.disabled = false
+          el.classList.remove('failed')
+        }, 2000)
         console.warn("Failed to copy content.")
       }
     );
